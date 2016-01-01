72b0fd354163fda41a7dd2ba2a0e10f5d1e8fdc3
Brian Rivas <brian.rivas64@gmail.com>
2016-01-01 14:08:02 -0800

    changed level spawner to pick random spawn points

--- necromancer/levels/level.py
+++ necromancer/levels/level.py
@@ -2,19 +2,18 @@ from pygame.color import THECOLORS
 
 from necromancer.util import Vector2
 from necromancer.units import Unit
+from random import Random
 
 class Level(object):
 
-
 	def __init__(self, board):
+		self.rand = Random("wosk")
 		self.spawns = {}
 		self.color = THECOLORS["red"]
-		self.add(0, (10, 0))
-		self.add(0, (10, 1))
-		self.add(1, (10, 2))
-		self.add(1, (10, 3))
-		self.add(2, (10, 4))
-		self.add(2, (10, 5))
+		self.spawns[0] = 1
+		self.spawns[1] = 3
+		self.spawns[2] = 1
+		self.spawns[3] = 2
 
 	def turn(self, game, unit):
 		enemy = unit.getNearestEnemy()
@@ -24,18 +23,17 @@ class Level(object):
 			unit.attack(enemy.pos)
 		if not unit.hasMoved:
 			unit.moveToward(enemy.pos)
-
-	def add(self, turn, toSpawn):
-		if not self.spawns.has_key(turn):
-			self.spawns[turn] = []
 			
-		turn_spawns = self.spawns[turn]
-		turn_spawns.append(toSpawn)
-		
 	def update(self, turn, board):
 		if not self.spawns.has_key(turn):
 			return
 			
 		toSpawn = self.spawns[turn]
-		for unit in toSpawn:
-			board.units.add(Unit(board, Vector2(unit[0], unit[1]), self))
+		for i in xrange(toSpawn):
+			board.units.add(Unit(board, self.getSpawnPoint(board), self))
+			
+	def getSpawnPoint(self, board):
+		pos = Vector2(10, self.rand.randint(0, 5))
+		while(board.getEntity(pos) is not None):
+			pos = Vector2(10, self.rand.randint(0, 5))
+		return pos
